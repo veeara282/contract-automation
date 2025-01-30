5f23b8add4dde9338820c6ee4ef2c7643a03ffd7
Evelyn <evelyn.ciara.345@gmail.com>
2025-01-29 21:50:51 -0500

    Use mdformat text wrapping

--- generate_contract.py
+++ generate_contract.py
@@ -1,4 +1,5 @@
 from jinja2 import Environment, PackageLoader, select_autoescape
+import mdformat
 from yaml import load, Loader
 
 
@@ -7,7 +8,7 @@ def main():
     with open("contract_params.yml", "r") as config_file:
         config = load(config_file, Loader=Loader)
 
-    print(config)
+    # print(config)
 
     # Look for the "template parameter" and attempt to load that template
     template_name = f'{config["template"]}.md.j2'
@@ -20,7 +21,20 @@ def main():
     template = env.get_template(template_name)
 
     rendered_document = template.render(config)
-    print(rendered_document)
+
+    # Jinja may generate very long lines or extraneous line breaks between paragraphs,
+    # so we use mdformat to wrap long lines and remove sequences of more than two
+    # consecutive line breaks. Note that mdformat does not wrap text by default.
+    # Wraps lines to 88 characters, like Black does.
+    # https://black.readthedocs.io/en/stable/the_black_code_style/current_style.html#line-length
+    formatted_document = mdformat.text(
+        rendered_document,
+        options={
+            "wrap": 88,
+        },
+    )
+
+    print(formatted_document)
 
 
 if __name__ == "__main__":
